d9d2f55e7118b22ac0e729df1614b4b7785a120c
Jota Santos <jailsonsantosdpge@outlook.com>
2024-06-12 13:20:59 -0400

    criando form de cadastro de usuario.

--- src/views/usuarios/UsuariosCreate.vue
+++ src/views/usuarios/UsuariosCreate.vue
@@ -1,40 +1,87 @@
 <template>
 	<div>
 		<div class="row">
-			<div class="col-12 col-xl-12">				
+			<div class="col-12 col-xl-12">
+
 				<div class="card">
 					<div class="card-header">
 						<h5 class="card-title">{{ tituloPagina }}</h5>
 						<h6 class="card-subtitle text-muted">
 							{{ descricaoPagina }}
 						</h6>
-					</div>					
+					</div>
+					<div class="card-body">
+						<div class="col-md-12">
+							<form @submit.prevent="store">
+								<div class="row">
+									<div class="mb-3 col com-sm-12 com-sm-12">
+										<label class="form-label" for="username">Username</label>
+										<input type="text" class="form-control" v-model="usuario.username" id="username" placeholder="Username">
+									</div>
+									<div class="mb-3 col com-sm-12 com-sm-12">
+										<label class="form-label" for="email">Email</label>
+										<input type="email" class="form-control" v-model="usuario.email" id="email"
+											placeholder="Email">
+									</div>
+									<div class="mb-3 col com-sm-12 com-sm-12">
+										<label class="form-label" for="role">Role</label>										
+										<select id="role" class="form-control" v-model="usuario.role">
+											<option selected="" disabled>Selecione...</option>
+											<option>...</option>
+										</select>
+									</div>
+									<div class="mb-3 col com-sm-12 com-sm-12">
+										<label class="form-label" for="inputPassword4">Password</label>
+										<input type="password" class="form-control" v-model="usuario.password" id="inputPassword4"
+											placeholder="Password">
+									</div>
+								</div>																
+								<button type="submit" class="btn btn-primary">Salvar</button>
+							</form>
+						</div>
+					</div>
 				</div>
-				<router-link class="btn btn-sm btn-primary mb-2" to="/usuarios">Voltar</router-link>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+import messageService from '@/services/messageService';
 
 export default {
 	name: "UsuariosCreate",
 	components: {
-		
+
 	},
 	data() {
 		return {
-			usuarios: [],
+			usuario: {
+				username: '',
+				password: '',
+				email: '',
+				role: ''
+			},
 			tituloPagina: 'Cadastrar novo usuário',
 			descricaoPagina: 'Preencha todos os dados corretamente'
 		};
 	},
 	created() {
-		
+
 	},
 	methods: {
-		
+		store() {
+			messageService.success("Usuário cadastrado com sucesso")
+			console.log('salvar');
+		},
+
+		clearForm() {
+			console.log('limpar form');
+			this.usuario.username = '';
+			this.usuario.password = '';
+			this.usuario.email = '';
+			this.usuario.role = '';
+		}
 	},
 };
 </script>
